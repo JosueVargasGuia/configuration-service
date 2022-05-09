3b4a1a55d47d73854d313cbe238576eab4b009d7
JostinSamuel <jostinmartinez1605@gmail.com>
2022-05-09 18:34:39 -0500

    tests

--- src/main/java/com/nttdata/configurationservice/controller/ConfigurationController.java
+++ src/main/java/com/nttdata/configurationservice/controller/ConfigurationController.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.reactive.function.server.ServerResponse;
 
 import com.nttdata.configurationservice.entity.Configuration;
 import com.nttdata.configurationservice.service.ConfigurationService;
@@ -34,39 +33,41 @@ public class ConfigurationController {
 	}
 
 	@PostMapping
-	public Mono<ResponseEntity<Configuration>> save(@RequestBody Configuration configuration) {
-		return configurationService.save(configuration).map(_configuration -> ResponseEntity.ok().body(_configuration))
-				.onErrorResume(e -> {
-					log.info("Error:" + e.getMessage());
-					return Mono.just(ResponseEntity.badRequest().build());
-				});
+	public Mono<Configuration> save(@RequestBody Configuration configuration) {
+		return configurationService.save(configuration);
+		/*.map(_configuration -> ResponseEntity.ok().body(_configuration))
+		.onErrorResume(e -> {
+			log.info("Error:" + e.getMessage());
+			return Mono.just(ResponseEntity.badRequest().build());
+		});*/
 	}
 
 	@GetMapping("/{idConfiguration}")
-	public Mono<ResponseEntity<Configuration>> findById(@PathVariable(name = "idConfiguration") long idConfiguration) {
-		return configurationService.findById(idConfiguration)
-				.map(configuration -> ResponseEntity.ok().body(configuration)).onErrorResume(e -> {
+	public Mono<Configuration> findById(@PathVariable(name = "idConfiguration") long idConfiguration) {
+		return configurationService.findById(idConfiguration);
+				/*.map(configuration -> ResponseEntity.ok().body(configuration)).onErrorResume(e -> {
 					log.info(e.getMessage());
 					return Mono.just(ResponseEntity.badRequest().build());
-				}).defaultIfEmpty(ResponseEntity.noContent().build());
+				}).defaultIfEmpty(ResponseEntity.noContent().build());*/
 	}
 
 	@PutMapping
-	public Mono<ResponseEntity<Configuration>> update(@RequestBody Configuration configuration) {
+	public Mono<Configuration> update(@RequestBody Configuration configuration) {
 
-		Mono<Configuration> mono = configurationService.findById(configuration.getIdConfiguration())
+		/*Mono<Configuration> mono = configurationService.findById(configuration.getIdConfiguration())
 				.flatMap(objConfiguration -> {
-					log.info("Update:[new]" + configuration + " [Old]:" + objConfiguration);
+					log.info("Update:[new]" + configuration + " [Old]:" + objConfiguration);*/
 					return configurationService.update(configuration);
-				});
+			/*	});
+		return mono;*/
 
-		return mono.map(_configuration -> {
+		/*return mono.map(_configuration -> {
 			log.info("Status:" + HttpStatus.OK);
 			return ResponseEntity.ok().body(_configuration);
 		}).onErrorResume(e -> {
 			log.info("Status:" + HttpStatus.BAD_REQUEST + " menssage" + e.getMessage());
 			return Mono.just(ResponseEntity.badRequest().build());
-		}).defaultIfEmpty(ResponseEntity.noContent().build());
+		}).defaultIfEmpty(ResponseEntity.noContent().build());*/
 
 	}
 
